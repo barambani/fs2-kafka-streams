17ea60c278b4c1f73241c3c2e6861bc6ecb9ad1d
Itamar Ravid <iravid@iravid.com>
2018-07-05 23:49:28 +0300

    WIP: ColumnFamily support

--- src/main/scala/com/iravid/fs2/kafka/streams/KeyValueStore.scala
+++ src/main/scala/com/iravid/fs2/kafka/streams/KeyValueStore.scala
@@ -1,41 +1,71 @@
 package com.iravid.fs2.kafka.streams
 
 import cats.effect.Sync
+import cats.effect.concurrent.Ref
 import cats.implicits._
 import fs2.Stream
-import org.rocksdb.RocksDB
+import java.nio.charset.StandardCharsets
+import org.rocksdb.{ ColumnFamilyDescriptor, ColumnFamilyHandle => RocksDBColFHandle, RocksDB }
 import scodec.Codec
 import scodec.bits.BitVector
 
 import scala.collection.JavaConverters._
 
 trait KeyValueStore[F[_], K, V] {
+  type ColumnFamilyHandle
+
+  def columnFamilies: F[List[ColumnFamilyHandle]]
+  def createColumnFamily(name: String): F[ColumnFamilyHandle]
+
   def get(k: K): F[Option[V]]
+  def get(columnFamily: ColumnFamilyHandle, k: K): F[Option[V]]
 
   def getAll(ks: List[K]): F[Map[K, V]]
+  def getAll(columnFamily: ColumnFamilyHandle, ks: List[K]): F[Map[K, V]]
 
   def put(k: K, v: V): F[Unit]
+  def put(columnFamily: ColumnFamilyHandle, k: K, v: V): F[Unit]
 
   def delete(k: K): F[Unit]
+  def delete(columnFamily: ColumnFamilyHandle, k: K): F[Unit]
 
   def scan: Stream[F, (K, V)]
+  def scan(columnFamily: ColumnFamilyHandle): Stream[F, (K, V)]
 }
 
-class RocksDBKeyValueStore[F[_], K, V](rocksdb: RocksDB)(implicit F: Sync[F],
-                                                         K: Codec[K],
-                                                         V: Codec[V])
+class RocksDBKeyValueStore[F[_], K, V](
+  rocksdb: RocksDB,
+  rocksDbColumnFamilies: Ref[F, Map[Int, RocksDBColFHandle]],
+  defaultColumnFamily: RocksDBColFHandle)(implicit F: Sync[F], K: Codec[K], V: Codec[V])
     extends KeyValueStore[F, K, V] {
+  type ColumnFamilyHandle = Int
 
-  def get(k: K): F[Option[V]] = F.delay {
-    val serializedKey = K.encode(k).require.toByteArray
+  def columnFamilies: F[List[ColumnFamilyHandle]] =
+    rocksDbColumnFamilies.get.map(_.values.map(_.getID).toList)
+  def createColumnFamily(name: String) =
+    F.delay(
+        rocksdb.createColumnFamily(
+          new ColumnFamilyDescriptor(name.getBytes(StandardCharsets.UTF_8))))
+      .flatMap { handle =>
+        rocksDbColumnFamilies.update(_ + (handle.getID -> handle)).as(handle.getID)
+      }
 
-    Option(rocksdb.get(serializedKey))
-      .map(bytes => V.decodeValue(BitVector.view(bytes)).require)
-  }
+  def get0(h: RocksDBColFHandle, k: K) =
+    F.delay {
+      Option(rocksdb.get(h, K.encode(k).require.toByteArray))
+        .map(bytes => V.decodeValue(BitVector.view(bytes)).require)
+    }
 
-  def getAll(ks: List[K]): F[Map[K, V]] = F.delay {
+  def get(k: K): F[Option[V]] = get0(defaultColumnFamily, k)
+  def get(columnFamily: ColumnFamilyHandle, k: K): F[Option[V]] =
+    rocksDbColumnFamilies.get
+      .flatMap(
+        _.get(columnFamily)
+          .flatTraverse(get0(_, k)))
+
+  def getAll0(h: RocksDBColFHandle, ks: List[K]): F[Map[K, V]] = F.delay {
     val serializedKeys = ks.map(K.encode(_).require.toByteArray)
-    val result = rocksdb.multiGet(serializedKeys.asJava).asScala.toMap
+    val result = rocksdb.multiGet(List.fill(ks.size)(h).asJava, serializedKeys.asJava).asScala.toMap
 
     result.map {
       case (k, v) =>
@@ -43,20 +73,36 @@ class RocksDBKeyValueStore[F[_], K, V](rocksdb: RocksDB)(implicit F: Sync[F],
           V.decodeValue(BitVector.view(v)).require
     }
   }
+  def getAll(ks: List[K]) = getAll0(defaultColumnFamily, ks)
+  def getAll(columnFamily: ColumnFamilyHandle, ks: List[K]): F[Map[K, V]] =
+    rocksDbColumnFamilies.get.flatMap(_.get(columnFamily) match {
+      case Some(h) => getAll0(h, ks)
+      case None    => F.pure(Map())
+    })
 
-  def delete(k: K): F[Unit] = F.delay {
-    rocksdb.delete(K.encode(k).require.toByteArray)
+  def delete0(h: RocksDBColFHandle, k: K) = F.delay {
+    rocksdb.delete(h, K.encode(k).require.toByteArray)
   }
+  def delete(k: K) = delete0(defaultColumnFamily, k)
+  def delete(columnFamily: ColumnFamilyHandle, k: K) =
+    rocksDbColumnFamilies.get.flatMap(
+      _.get(columnFamily)
+        .traverse_(delete0(_, k)))
 
-  def put(k: K, v: V): F[Unit] = F.delay {
-    rocksdb.put(K.encode(k).require.toByteArray, V.encode(v).require.toByteArray)
+  def put0(h: RocksDBColFHandle, k: K, v: V) = F.delay {
+    rocksdb.put(h, K.encode(k).require.toByteArray, V.encode(v).require.toByteArray)
   }
+  def put(k: K, v: V) = put0(defaultColumnFamily, k, v)
+  def put(columnFamily: ColumnFamilyHandle, k: K, v: V) =
+    rocksDbColumnFamilies.get.flatMap(
+      _.get(columnFamily)
+        .traverse_(put0(_, k, v)))
 
-  def scan: Stream[F, (K, V)] =
+  def scan0(h: RocksDBColFHandle): Stream[F, (K, V)] =
     Stream
       .bracket {
         F.delay {
-          val iterator = rocksdb.newIterator()
+          val iterator = rocksdb.newIterator(h)
           iterator.seekToFirst()
           iterator
         }
@@ -75,4 +121,11 @@ class RocksDBKeyValueStore[F[_], K, V](rocksdb: RocksDB)(implicit F: Sync[F],
           }
         }.unNoneTerminate
       }
+
+  def scan: Stream[F, (K, V)] = scan0(defaultColumnFamily)
+  def scan(columnFamily: ColumnFamilyHandle): Stream[F, (K, V)] =
+    Stream.eval(rocksDbColumnFamilies.get.map(_.get(columnFamily))) flatMap {
+      case None    => Stream.empty
+      case Some(h) => scan0(h)
+    }
 }
--- src/main/scala/com/iravid/fs2/kafka/streams/KeyValueStores.scala
+++ src/main/scala/com/iravid/fs2/kafka/streams/KeyValueStores.scala
@@ -1,10 +1,13 @@
 package com.iravid.fs2.kafka.streams
 
+import cats.effect.concurrent.Ref
 import cats.effect.{ Resource, Sync }
 import cats.implicits._
 import java.nio.file.Path
-import org.rocksdb.RocksDB
+import org.rocksdb.{ ColumnFamilyDescriptor, ColumnFamilyHandle, Options, RocksDB }
 import scodec.Codec
+
+import scala.collection.JavaConverters._
 
 trait KeyValueStores[C[_], P, F[_]] {
   def open[K: C, V: C](storeKey: P): Resource[F, KeyValueStore[F, K, V]]
@@ -14,9 +17,54 @@ class RocksDBKeyValueStores[F[_]](implicit F: Sync[F]) extends KeyValueStores[Co
   def open[K: Codec, V: Codec](storeKey: Path): Resource[F, KeyValueStore[F, K, V]] =
     Resource
       .make {
-        F.delay {
-          RocksDB.open(storeKey.toAbsolutePath.toString)
-        }
-      }(rocksdb => F.delay(rocksdb.close()))
-      .map(rocksdb => new RocksDBKeyValueStore[F, K, V](rocksdb))
+        for {
+          cfNames <- F.delay(
+                      RocksDB.listColumnFamilies(new Options(), storeKey.toAbsolutePath.toString))
+          cfDescs = cfNames.asScala.map(new ColumnFamilyDescriptor(_))
+          storeAndHandles <- F.delay {
+                              val cfHandles = new java.util.ArrayList[ColumnFamilyHandle]()
+                              val store =
+                                RocksDB.open(
+                                  storeKey.toAbsolutePath.toString,
+                                  cfDescs.asJava,
+                                  cfHandles)
+
+                              (store, cfHandles.asScala.toList)
+                            }
+          (store, handles) = storeAndHandles
+          defaultAndRest <- {
+            val (before, after) =
+              handles.span(h => java.util.Arrays.equals(h.getName, RocksDB.DEFAULT_COLUMN_FAMILY))
+            val default = after.headOption
+            val rest = before ++ after.drop(1)
+
+            default match {
+              case Some(d) => F.pure((d, rest))
+              case None =>
+                F.raiseError[(ColumnFamilyHandle, List[ColumnFamilyHandle])](
+                  new Exception("Could not locate default column family!"))
+            }
+          }
+          (default, rest) = defaultAndRest
+          handlesRef <- Ref[F].of(rest.map(h => h.getID -> h).toMap)
+        } yield (store, handlesRef, default)
+      } {
+        case (rocksdb, columnFamilyHandles, defaultColumnFamily) =>
+          for {
+            handles <- columnFamilyHandles.get
+            _ <- handles.values.toList.traverse_ { handle =>
+                  F.delay(handle.close())
+                }
+            _ <- F.delay(defaultColumnFamily.close())
+            _ <- F.delay(rocksdb.close())
+          } yield ()
+      }
+      .map {
+        case (rocksdb, columnFamilyHandles, defaultColumnFamily) =>
+          new RocksDBKeyValueStore(
+            rocksdb,
+            columnFamilyHandles,
+            defaultColumnFamily
+          )
+      }
 }
