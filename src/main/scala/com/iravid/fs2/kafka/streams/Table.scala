d030fd2c005bb3038f7ac1825906eed81b873339
Itamar Ravid <iravid@iravid.com>
2018-07-09 23:34:01 +0300

    Experiments

--- src/main/scala/com/iravid/fs2/kafka/streams/Table.scala
+++ src/main/scala/com/iravid/fs2/kafka/streams/Table.scala
@@ -1,15 +1,75 @@
 package com.iravid.fs2.kafka.streams
 
+import cats.Functor
+import cats.implicits._
+import cats.effect.concurrent.Ref
 import scodec.{ Attempt, Codec, Err }
 
 trait Table[F[_], K, V] {
   def putAll(data: List[(K, V)]): F[Unit]
 
-  def getAll(k: List[K]): F[Map[K, V]]
+  def getAll(k: List[K]): F[Map[K, Option[V]]]
 
   def commit(offset: Long): F[Unit]
 
   def delete(k: K): F[Unit]
+}
+
+object InMemoryTable {
+  import cats.data.Kleisli
+
+  case class State[K, V](data: Map[K, V], offset: Long)
+
+  def kleisli[F[_]: Functor, K, V]: Table[Kleisli[F, Ref[F, State[K, V]], ?], K, V] =
+    new Table[Kleisli[F, Ref[F, State[K, V]], ?], K, V] {
+      def commit(offset: Long): Kleisli[F, Ref[F, State[K, V]], Unit] =
+        Kleisli(_.update(state => state.copy(offset = offset)))
+
+      def delete(k: K): Kleisli[F, Ref[F, State[K, V]], Unit] =
+        Kleisli(_.update(state => state.copy(data = state.data - k)))
+
+      def getAll(k: List[K]): Kleisli[F, Ref[F, State[K, V]], Map[K, Option[V]]] =
+        Kleisli(_.get.map(state => k.fproduct(state.data.get).toMap))
+
+      def putAll(data: List[(K, V)]): Kleisli[F, Ref[F, State[K, V]], Unit] =
+        Kleisli(_.update(state => state.copy(data = state.data ++ data)))
+    }
+
+  trait MonadState[F[_], S] {
+    def get: F[S]
+    def set: F[Unit]
+    def update(f: S => S): F[Unit]
+  }
+
+  def mtl[F[_]: Functor, K, V](implicit S: MonadState[F, State[K, V]]): Table[F, K, V] =
+    new Table[F, K, V] {
+      def commit(offset: Long): F[Unit] =
+        S.update(state => state.copy(offset = offset))
+
+      def delete(k: K): F[Unit] =
+        S.update(state => state.copy(data = state.data - k))
+
+      def getAll(k: List[K]): F[Map[K, Option[V]]] =
+        S.get.map(state => k.fproduct(state.data.get).toMap)
+
+      def putAll(data: List[(K, V)]): F[Unit] =
+        S.update(state => state.copy(data = state.data ++ data))
+    }
+}
+
+class InMemoryTable[F[_]: Functor, K, V](ref: Ref[F, InMemoryTable.State[K, V]])
+    extends Table[F, K, V] {
+  def putAll(data: List[(K, V)]): F[Unit] =
+    ref.update(state => state.copy(data = state.data ++ data))
+
+  def getAll(k: List[K]): F[Map[K, Option[V]]] =
+    ref.get.map(state => k.fproduct(state.data.get).toMap)
+
+  def commit(offset: Long): F[Unit] =
+    ref.update(state => state.copy(offset = offset))
+
+  def delete(k: K): F[Unit] =
+    ref.update(state => state.copy(data = state.data - k))
 }
 
 object RocksDBTable {
@@ -30,16 +90,20 @@ object RocksDBTable {
   }
 }
 
-class RocksDBTable[F[_], K: Codec, V: Codec, CF](store: PolyKVStore.Aux[F, Codec, CF],
-                                                 dataCF: CF,
-                                                 offsetsCF: CF) {
+class RocksDBTable[F[_]: Functor, K: Codec, V: Codec, CF](store: PolyKVStore.Aux[F, Codec, CF],
+                                                          dataCF: CF,
+                                                          offsetsCF: CF)
+    extends Table[F, K, V] {
   import RocksDBTable._
 
   implicit val K: Key.Aux[Codec, K, V] = Key.instance
 
   def putAll(data: List[(K, V)]): F[Unit] = store.putAll(dataCF, data)
 
-  def getAll(ks: List[K]): F[Map[K, V]] = store.getAll(dataCF, ks)
+  def getAll(ks: List[K]): F[Map[K, Option[V]]] =
+    store.getAll(dataCF, ks).map { retrievedMap =>
+      ks.fproduct(retrievedMap.get).toMap
+    }
 
   def delete(k: K): F[Unit] = store.delete(dataCF, k)
 
